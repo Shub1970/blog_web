1a6aedbaf99575469e5d227c566f3565b506d8d6
Shub1970 <18shubham1998@gmail.com>
2024-01-10 10:27:37 +0530

    finaly done

--- public/main.js
+++ public/main.js
@@ -1,1 +1,25 @@
+const mobile_nav_toggle = document.querySelector(".mobile-nav-toggle");
+const icon_close = document.querySelector(".icon-close");
+const hamburder = document.querySelector(".hamburger");
 
+mobile_nav_toggle.addEventListener("click", function () {
+  if (this.getAttribute("aria-expanded") == "false") {
+    this.setAttribute("aria-expanded", "true");
+    hamburder.style.display = "none";
+    icon_close.style.display = "block";
+  } else {
+    this.setAttribute("aria-expanded", "false");
+    hamburder.style.display = "block";
+    icon_close.style.display = "none";
+  }
+});
+
+const links = document.querySelectorAll(".main-nav a[aria-expanded]");
+links.forEach((link) => {
+  link.addEventListener("click", function () {
+    links.forEach((link) => {
+      link.setAttribute("aria-expanded", "false");
+    });
+    this.setAttribute("aria-expanded", "true");
+  });
+});
